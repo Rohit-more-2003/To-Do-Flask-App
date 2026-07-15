6211ca4a87961f7fd1abebe7a95b0db4ada6b6ab
Rohit_More <morer4851@gmail.com>
2026-07-16 00:52:59 +0530

    Commited data to database

--- routes.py
+++ routes.py
@@ -1,20 +1,27 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from datetime import datetime, UTC
 
-from app import app
+from app import app, db
 import forms
+from models import Task
 
 @app.route('/')
 def index():
-    return render_template('index.html', current_title="Custom Title")
+    tasks = Task.query.all()
+
+    return render_template('index.html', tasks=tasks)
 
 
-@app.route('/add', methods=['GET', 'POST']) # Deafault is methods=['POST']
+@app.route('/add', methods=['GET', 'POST']) # Default is methods=['POST']
 def add_task():
     form = forms.AddTaskForm()
 
     if form.validate_on_submit():
-        print('Submitted title', form.title.data)
+        # print('Submitted title', form.title.data)
+        task = Task(title=form.title.data, date=datetime.now(UTC).date())
+        db.session.add(task)
+        db.session.commit()
 
-        return render_template('add.html', form=form, title=form.title.data)
+        return redirect(url_for('index'))
 
     return render_template('add.html', form=form)
