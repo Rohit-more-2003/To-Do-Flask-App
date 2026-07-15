077f09446b2b051e9b3478af45a529339b311014
Rohit_More <morer4851@gmail.com>
2026-07-15 21:11:44 +0530

    Handled POST Requests

--- routes.py
+++ routes.py
@@ -8,7 +8,13 @@ def index():
     return render_template('index.html', current_title="Custom Title")
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST']) # Deafault is methods=['POST']
 def add_task():
     form = forms.AddTaskForm()
+
+    if form.validate_on_submit():
+        print('Submitted title', form.title.data)
+
+        return render_template('add.html', form=form, title=form.title.data)
+
     return render_template('add.html', form=form)
